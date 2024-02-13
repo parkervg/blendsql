63c944093496cf7361c074ac4fa5c88030bbf921
parkervg <parkervg5@gmail.com>
2024-02-13 11:24:49 -0500

    Making llm importable via `blendsql`

--- blendsql/__init__.py
+++ blendsql/__init__.py
@@ -1,5 +1,6 @@
 __version__ = "0.0.1"
 
 from .ingredients.builtin import LLMMap, LLMQA, LLMJoin, DT
+from .ingredients.builtin import llm
 from .blendsql import blend
 from .utils import init_secrets
