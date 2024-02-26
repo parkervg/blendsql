a4d1626668fc98d321f45f92dcf34f0dfd7e13e8
parkervg <parkervg5@gmail.com>
2024-02-26 15:03:49 -0500

    Better debug query

--- research/run-debug.py
+++ research/run-debug.py
@@ -1,64 +1,33 @@
-from blendsql import LLMMap, LLMQA, LLMJoin, blend, init_secrets
+from blendsql import LLMMap, LLMQA, LLMJoin, blend
 from blendsql.db import SQLiteDBConnector
-
-init_secrets("../secrets.json")
+from blendsql.llms import AzureOpenaiLLM
 
 if __name__ == "__main__":
-    #  blendsql = """
-    # {{
-    #  LLMQA(
-    #      'What were the position held by William Warren Barbour during his life?',
-    #      (
-    #           SELECT * from w WHERE vacator like 'william warren barbour%';
-    #      )
-    #  )
-    #  }}
-    #  """
-    #  db_path = "research/db/fetaqa/totto_source/train_json/example-0.db"
-
-    # blendsql = """
-    # {{
-    #   LLMQA(
-    #         'How is Dyro ranked in the year of 2014',
-    #             (
-    #                 SELECT category, result FROM w WHERE nominee = 'dyro' AND year = '2014'
-    #             )
-    #         )
-    #  }}
-    #  """
-    # db_path = "research/db/fetaqa/totto_source/train_json/example-4880.db"
-
-    # blendsql = """
-    # {{
-    #  LLMQA(
-    #        'For what work does Andy Karl win his  Olivier Award?',
-    #            (
-    #                SELECT year, work FROM w WHERE award like '%Olivier Award';
-    #            )
-    #        )
-    # }}
-    # """
-    # db_path = "research/db/fetaqa/totto_source/dev_json/example-2274.db"
-
     blendsql = """
        {{
-        LLMQA(
-              'In what films did Pooja Ramachandran play Cathy?',
-                  (
-                      SELECT film FROM w WHERE role='cathy';
-                  )
-              )
-       }}
+            LLMQA(
+                'What is the middle name of this player?',
+                (
+                    SELECT documents.title AS 'Player', documents.content FROM documents
+                    JOIN {{
+                        LLMJoin(
+                            left_on='w::player',
+                            right_on='documents::title'
+                        )
+                    }} WHERE w."rank" = 2
+                )
+            )
+        }}
        """
-    db_path = "research/db/fetaqa/totto_source/train_json/example-2954.db"
+    db_path = "research/db/hybridqa/List_of_National_Football_League_rushing_yards_leaders_0.db"
 
     db = SQLiteDBConnector(db_path)
     smoothie = blend(
         query=blendsql,
         db=db,
         ingredients={LLMMap, LLMQA, LLMJoin},
-        overwrite_args={"use_endpoint": "gpt-4", "long_answer": True},
-        infer_map_constraints=True,
+        blender=AzureOpenaiLLM("gpt-4"),
+        infer_gen_constraints=True,
         silence_db_exec_errors=False,
         verbose=True,
     )
