c437139a497aa252f0867115b5fc445073159ebd
parkervg <parkervg5@gmail.com>
2024-03-14 15:33:17 -0400

    Overdue fix: removing unused model constants

--- blendsql/_constants.py
+++ blendsql/_constants.py
@@ -9,17 +9,6 @@ class StrInMeta(EnumMeta):
         return item in cls.__members__.values()
 
 
-OPENAI_COMPLETE_LLM = ["text-davinci-003"]
-OPENAI_CHAT_LLM = [
-    "gpt-4",
-    "gpt-4-32k",
-    "gpt-4-32k-0613",
-    "gpt-35-turbo",
-    "gpt-35-turbo-0613",
-    "gpt-35-turbo-16k-0613",
-    "gpt-35-turbo-instruct-0914",
-]
-
 DEFAULT_ANS_SEP = ";"
 DEFAULT_NAN_ANS = "-"
 VALUE_BATCH_SIZE = 10
