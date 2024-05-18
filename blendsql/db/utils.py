2fa6f98cab7911cc39634806671e61234f8a3ba9
parkervg <parkervg5@gmail.com>
2024-05-18 13:28:47 -0400

    applymap -> map

--- blendsql/db/utils.py
+++ blendsql/db/utils.py
@@ -20,7 +20,7 @@ def select_all_from_table_query(tablename: str) -> str:
 
 def truncate_df_content(df: pd.DataFrame, truncation_limit: int) -> pd.DataFrame:
     # Truncate long strings
-    return df.applymap(
+    return df.map(
         lambda x: f"{str(x)[:truncation_limit]}..."
         if isinstance(x, str) and len(str(x)) > truncation_limit
         else x
