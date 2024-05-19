17385e72b765cfb4af4aa2ec3fa2d835a84aee06
parkervg <parkervg5@gmail.com>
2024-05-19 11:20:59 -0400

    Allow empty ingredient arg, add blendsql_scalar_functions

--- blendsql/grammars/utils.py
+++ blendsql/grammars/utils.py
@@ -1,6 +1,8 @@
+import logging
 from pathlib import Path
-from typing import Optional, Collection, List, Dict
+from typing import Optional, Collection, List, Dict, Type
 from string import Template
+from colorama import Fore
 
 from ..ingredients import Ingredient
 from .._constants import IngredientType
@@ -19,17 +21,27 @@ def format_ingredient_names_to_lark(names: List[str]) -> str:
     return "(" + " | ".join([f'"{n}("i' for n in names]) + ")"
 
 
-def load_cfg_parser(ingredients: Optional[Collection[Ingredient]]) -> EarleyParser:
+def load_cfg_parser(
+    ingredients: Optional[Collection[Type[Ingredient]]] = None,
+) -> EarleyParser:
     """Loads BlendSQL CFG parser.
     Dynamically modifies grammar string to include only valid ingredients.
     """
+    if ingredients is None:
+        logging.debug(
+            Fore.YELLOW
+            + "No ingredients passed to `load_cfg_parser()`!\nWas this on purpose?"
+        )
+        ingredients = set()
     with open(Path(__file__).parent / "./_cfg_grammar.lark", encoding="utf-8") as f:
         cfg_grammar = Template(f.read())
     blendsql_join_functions = []
     blendsql_aggregate_functions = []
+    blendsql_scalar_functions = []
     ingredient_type_to_function_type: Dict[str, List[str]] = {
         IngredientType.JOIN: blendsql_join_functions,
         IngredientType.QA: blendsql_aggregate_functions,
+        IngredientType.MAP: blendsql_scalar_functions,
     }
     for ingredient in ingredients:
         if ingredient.ingredient_type not in ingredient_type_to_function_type:
@@ -45,6 +57,9 @@ def load_cfg_parser(ingredients: Optional[Collection[Ingredient]]) -> EarleyPars
         blendsql_aggregate_functions=format_ingredient_names_to_lark(
             blendsql_aggregate_functions
         ),
+        blendsql_scalar_functions=format_ingredient_names_to_lark(
+            blendsql_scalar_functions
+        ),
     )
     return EarleyParser(
         grammar=cfg_grammar,
@@ -57,5 +72,5 @@ if __name__ == "__main__":
     from blendsql import LLMMap, LLMJoin, LLMValidate
 
     parser = load_cfg_parser({LLMMap, LLMJoin, LLMValidate})
-    # parser.parse("{{LLMQA('what is the answer', (select * from w))}}")
+    parser.parse("{{LLMQA('what is the answer', (select * from w))}}")
     print()
