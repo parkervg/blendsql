c5eec8dfd24b743d40c40400e7d7d7b9ac89ffd2
parkervg <parkervg5@gmail.com>
2024-05-23 17:31:32 -0400

    Getting llamacpp working with outlines

--- blendsql/models/local/_llama_cpp.py
+++ blendsql/models/local/_llama_cpp.py
@@ -1,21 +1,21 @@
 import importlib.util
-from guidance.models import LlamaCpp
-from guidance.models import Model as GuidanceModel
+from outlines.models import llamacpp, LogitsGenerator
 
-
-from .._model import Model
+from .._model import LocalModel
 
 _has_llama_cpp = importlib.util.find_spec("llama_cpp") is not None
 
 
-class LlamaCppLLM(Model):
+class LlamaCppLLM(LocalModel):
     """Class for llama-cpp local Model.
 
     Args:
         model_name_or_path: Name of the model on HuggingFace, or the path to a local model
     """
 
-    def __init__(self, model_name_or_path: str, caching: bool = True, **kwargs):
+    def __init__(
+        self, model_name_or_path: str, filename: str, caching: bool = True, **kwargs
+    ):
         if not _has_llama_cpp:
             raise ImportError(
                 "Please install llama_cpp with `pip install llama-cpp-python`!"
@@ -23,12 +23,12 @@ class LlamaCppLLM(Model):
 
         super().__init__(
             model_name_or_path=model_name_or_path,
-            requires_config=False,
             # TODO: how to get llama_cpp tokenizer?
             tokenizer=None,
+            requires_config=False,
+            load_model_kwargs=kwargs | {"filename": filename},
             caching=caching,
-            **kwargs
         )
 
-    def _load_model(self) -> GuidanceModel:
-        return LlamaCpp(self.model_name_or_path, n_ctx=4000, echo=False)
+    def _load_model(self, filename: str, **kwargs) -> LogitsGenerator:
+        return llamacpp(self.model_name_or_path, filename=filename, **kwargs)
