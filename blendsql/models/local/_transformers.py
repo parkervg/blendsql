711dff893250bd158727d5df5eb5a3d619907cf9
parkervg <parkervg5@gmail.com>
2024-06-06 16:35:07 -0400

    Working out transformers + torch logic

--- blendsql/models/local/_transformers.py
+++ blendsql/models/local/_transformers.py
@@ -4,6 +4,7 @@ from outlines.models import transformers, LogitsGenerator
 from .._model import LocalModel
 
 _has_transformers = importlib.util.find_spec("transformers") is not None
+_has_torch = importlib.util.find_spec("torch") is not None
 
 
 class TransformersLLM(LocalModel):
@@ -21,9 +22,17 @@ class TransformersLLM(LocalModel):
     """
 
     def __init__(self, model_name_or_path: str, caching: bool = True, **kwargs):
-        if not _has_transformers:
+        if not _has_transformers and _has_torch:
             raise ImportError(
                 "Please install transformers with `pip install transformers`!"
+            ) from None
+        elif not _has_torch and _has_transformers:
+            raise ImportError(
+                "Please install pytorch with `pip install torch`!"
+            ) from None
+        elif not _has_torch and not _has_transformers:
+            raise ImportError(
+                "Please install transformers and pytorch with `pip install transformers torch`!"
             ) from None
         import transformers
 
