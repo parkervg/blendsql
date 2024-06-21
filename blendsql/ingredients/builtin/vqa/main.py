42e01bbd32d8a203db5c9b4b8344c7dc812217f1
parkervg <parkervg5@gmail.com>
2024-06-20 22:36:16 -0400

    ImageCaptionProgram

--- blendsql/ingredients/builtin/vqa/main.py
+++ blendsql/ingredients/builtin/vqa/main.py
@@ -1,25 +1,30 @@
-from typing import List
+from typing import List, Tuple
+from PIL import Image
+from io import BytesIO
 
 from blendsql.models import Model
+from blendsql._program import Program
 from blendsql.ingredients.ingredient import MapIngredient
 from blendsql._exceptions import IngredientException
 
 
-class ImageCaption(MapIngredient):
-    def run(self, values: List[bytes], **kwargs):
-        """Generates a caption for all byte images passed to it."""
-        if not all(isinstance(value, bytes) for value in values):
-            raise IngredientException(f"All values must be 'byte' type for LlavaVQA!")
-        return self.model.predict(img_bytes=values)
-
-    @staticmethod
-    def predict(model: Model, img_bytes: List[bytes]):
-        model_output = model.logits_generator(
+class ImageCaptionProgram(Program):
+    def __call__(
+        self, model: Model, img_bytes: List[bytes], **kwargs
+    ) -> Tuple[List[str], str]:
+        model_output = model.model_obj(
             images=[Image.open(BytesIO(value)) for value in img_bytes],
             # prompt=prompt,
             generate_kwargs={"max_new_tokens": 200},
         )
-        return [
-            output[0]["generated_text"].lstrip(prompt).strip()
-            for output in model_output
-        ]
+        return ([output[0]["generated_text"].strip() for output in model_output], "")
+
+
+class ImageCaption(MapIngredient):
+    def run(self, model: Model, values: List[bytes], **kwargs):
+        """Generates a caption for all byte images passed to it."""
+        if not all(isinstance(value, bytes) for value in values):
+            raise IngredientException(
+                f"All values must be 'byte' type for ImageCaption!"
+            )
+        return model.predict(program=ImageCaptionProgram, img_bytes=values, **kwargs)
