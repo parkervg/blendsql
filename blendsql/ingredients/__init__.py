4c7c451668dbe69ee5b4d75e02be41cd5a7149c5
parkervg <parkervg5@gmail.com>
2024-09-26 16:43:36 -0400

    Fixing ModuleNotFound error when installing with pip

--- blendsql/ingredients/__init__.py
+++ blendsql/ingredients/__init__.py
@@ -1,10 +1,1 @@
-from .ingredient import (
-    Ingredient,
-    MapIngredient,
-    JoinIngredient,
-    StringIngredient,
-    QAIngredient,
-    IngredientException,
-)
-
 from .builtin import LLMQA, LLMJoin, LLMMap, LLMValidate, ImageCaption
