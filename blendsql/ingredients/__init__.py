31e85678f5c5bb6878f67b96440c6c9c8ef492d7
parkervg <parkervg5@gmail.com>
2024-09-26 16:51:40 -0400

    pycharm lied to me

--- blendsql/ingredients/__init__.py
+++ blendsql/ingredients/__init__.py
@@ -1,1 +1,9 @@
 from .builtin import LLMQA, LLMJoin, LLMMap, LLMValidate, ImageCaption
+from .ingredient import (
+    Ingredient,
+    MapIngredient,
+    JoinIngredient,
+    StringIngredient,
+    QAIngredient,
+    IngredientException,
+)
