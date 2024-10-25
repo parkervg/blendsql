5390793bab3b1b2aa5a97bd2710b5514e77f1e03
parkervg <parkervg5@gmail.com>
2024-10-24 22:52:35 -0400

    Integrating DataType classes to examples.py

--- blendsql/ingredients/builtin/qa/examples.py
+++ blendsql/ingredients/builtin/qa/examples.py
@@ -1,10 +1,10 @@
 from attr import attrs, attrib
 import pandas as pd
-from typing import Optional, Callable, Literal
+from typing import Optional, Callable
 from collections.abc import Collection
 
 from blendsql.ingredients.few_shot import Example
-from blendsql._constants import ModifierType
+from blendsql._constants import DataType, STR_TO_DATATYPE
 
 
 @attrs(kw_only=True)
@@ -15,19 +15,10 @@ class QAExample(Example):
         default=None,
     )
     options: Optional[Collection[str]] = attrib(default=None)
-    output_type: Optional[
-        Literal[
-            "boolean",
-            "integer",
-            "float",
-            "string",
-            "List[boolean]",
-            "List[integer]",
-            "List[float]",
-            "List[string",
-        ]
-    ] = attrib(default=None)
-    modifier: ModifierType = attrib(default=None)
+    output_type: DataType = attrib(
+        converter=lambda s: STR_TO_DATATYPE[s] if isinstance(s, str) else s,
+        default=None,
+    )
 
     def to_string(
         self,
@@ -39,27 +30,30 @@ class QAExample(Example):
         s = ""
         s += f"\n\nQuestion: {self.question}\n"
         if self.output_type is not None:
-            s += f"Output datatype: {self.output_type}\n"
+            if self.output_type.name != "Any":
+                s += f"Output datatype: {self.output_type.name}\n"
         if list_options:
             if self.options is not None:
                 s += f"Options: {', '.join(sorted(self.options))}\n"
-        if self.modifier is not None:
-            if self.modifier == "*":
-                s += "You may generate zero or more responses in your list.\n"
-            elif self.modifier == "+":
-                s += "You may generate one or more responses in your list.\n"
-            else:
-                repeats = [
-                    int(i)
-                    for i in self.modifier.replace("}", "").replace("{", "").split(",")
-                ]
-                if len(repeats) == 1:
-                    repeats = repeats * 2
-                min_length, max_length = repeats
-                if min_length == max_length:
-                    s += f"You may generate {min_length} responses in your list.\n"
+        if self.output_type is not None:
+            modifier = self.output_type.modifier
+            if modifier is not None:
+                if modifier == "*":
+                    s += "You may generate zero or more responses in your list.\n"
+                elif modifier == "+":
+                    s += "You may generate one or more responses in your list.\n"
                 else:
-                    s += f"You may generate between {min_length} and {max_length} responses in your list.\n"
+                    repeats = [
+                        int(i)
+                        for i in modifier.replace("}", "").replace("{", "").split(",")
+                    ]
+                    if len(repeats) == 1:
+                        repeats = repeats * 2
+                    min_length, max_length = repeats
+                    if min_length == max_length:
+                        s += f"You may generate {min_length} responses in your list.\n"
+                    else:
+                        s += f"You may generate between {min_length} and {max_length} responses in your list.\n"
         if self.context is not None:
             s += f"Context:\n{context_formatter(self.context)}"
         s += "\nAnswer: "
