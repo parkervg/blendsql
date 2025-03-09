acb534441ebb138a08268994a59642d1ff32ccd9
Rohit Khoja <32352533+rohitkhoja@users.noreply.github.com>
2025-03-09 12:04:04 -0700

    adding gemini __init__.py

--- blendsql/models/__init__.py
+++ blendsql/models/__init__.py
@@ -3,4 +3,5 @@ from .local._azure_phi import AzurePhiModel
 from .remote._ollama import OllamaLLM
 from .remote._openai import OpenaiLLM, AzureOpenaiLLM
 from .remote._anthropic import AnthropicLLM
+from .remote._gemini import GeminiLLM  # Add this line
 from ._model import Model, RemoteModel, LocalModel, ModelObj
