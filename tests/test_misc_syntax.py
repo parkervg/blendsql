4f63531f59bd02e1f3b3d44085c8531f1d036818
parkervg <parkervg5@gmail.com>
2025-04-15 16:34:27 -0400

    dropping `_blendsql` suffix in tests

--- tests/test_misc_syntax.py
+++ tests/test_misc_syntax.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 from blendsql import BlendSQL, config
 from blendsql.ingredients import LLMQA, LLMMap, LLMJoin
+from .utils import select_first_option
 
 config.set_async_limit(1)
 
@@ -13,7 +14,7 @@ def bsql() -> BlendSQL:
         {
             "League": pd.DataFrame(
                 [
-                    {"id": 1, "country_id": 1, "name": "Belgium Jupiler League"},
+                    {"id": 1, "country_id": 1, "name": "Z Belgium Jupiler League"},
                     {"id": 1729, "country_id": 1729, "name": "England Premier League"},
                     {"id": 4769, "country_id": 4769, "name": "France Ligue 1"},
                     {"id": 7809, "country_id": 7809, "name": "Germany 1. Bundesliga"},
@@ -30,7 +31,7 @@ def bsql() -> BlendSQL:
                 ]
             ),
         },
-        ingredients={LLMQA, LLMMap, LLMJoin},
+        ingredients={LLMQA, LLMMap, LLMJoin, select_first_option},
     )
 
 
@@ -43,3 +44,15 @@ def test_many_aliases(bsql, model):
         """,
         model=model,
     )
+
+
+def test_join_with_duplicate_columns(bsql, model):
+    smoothie = bsql.execute(
+        """
+        SELECT l.name FROM League l
+        JOIN Country c ON l.country_id = c.id
+        WHERE c.name = {{select_first_option('c::name')}}
+        """,
+        model=model,
+    )
+    assert smoothie.df.values.flat[0] == "Z Belgium Jupiler League"
