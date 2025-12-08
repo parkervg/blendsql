b6311ff857b9836649ed0df2d2e2e135136d9d86
parkervg <parkervg5@gmail.com>
2025-12-08 11:19:36 -0500

    add test_smoothie_meta.py
    Make sure we're counting meta stats correctly

---
+++ tests/test_smoothie_meta.py
@@ -0,0 +1,80 @@
+import pytest
+import pandas as pd
+
+from blendsql.ingredients import LLMMap
+from blendsql import BlendSQL, config
+
+config.set_async_limit(1)
+
+
+@pytest.fixture(scope="module")
+def bsql() -> BlendSQL:
+    return BlendSQL(
+        {
+            "People": pd.DataFrame(
+                {
+                    "Name": [
+                        "George Washington",
+                        "John Quincy Adams",
+                        "Thomas Jefferson",
+                        "James Madison",
+                        "James Monroe",
+                        "Alexander Hamilton",
+                        "Sabrina Carpenter",
+                        "Charli XCX",
+                        "Elon Musk",
+                        "Michelle Obama",
+                    ],
+                    "Known_For": [
+                        "Established federal government, First U.S. President",
+                        "XYZ Affair, Alien and Sedition Acts",
+                        "Louisiana Purchase, Declaration of Independence",
+                        "War of 1812, Constitution",
+                        "Monroe Doctrine, Missouri Compromise",
+                        "Created national bank, Federalist Papers",
+                        "Nonsense, Emails I Cant Send, Mean Girls musical",
+                        "Crash, How Im Feeling Now, Boom Clap",
+                        "Tesla, SpaceX, Twitter/X acquisition",
+                        "Lets Move campaign, Becoming memoir",
+                    ],
+                }
+            ),
+            "Eras": pd.DataFrame({"Years": ["1800-1900", "1900-2000", "2000-Now"]}),
+        },
+    )
+
+
+def test_num_values_passed_map(bsql, model):
+    # With the above db, this will be 10
+    total_num_values_to_process = bsql.db.execute_to_list(
+        "SELECT COUNT(DISTINCT Name) FROM People", to_type=int
+    )[0]
+    max_tokens = 1
+    for bs in [1, 5, 10]:
+        smoothie = bsql.execute(
+            f"""
+            SELECT {{{{LLMMap('Is a famous singer?', p.Name, max_tokens={max_tokens})}}}} FROM People p
+            """,
+            ingredients={LLMMap.from_args(batch_size=bs)},
+            model=model,
+        )
+        assert smoothie.meta.num_values_passed == total_num_values_to_process
+        assert smoothie.meta.num_generation_calls == total_num_values_to_process // bs
+        assert (
+            smoothie.meta.completion_tokens
+            == (max_tokens * total_num_values_to_process) // bs
+        )
+
+
+def test_num_values_passed_qa(bsql, model):
+    # With the above db, this will be 10
+    smoothie = bsql.execute(
+        """
+        SELECT * FROM People p 
+        WHERE p.Name = {{LLMQA('Who founded Tesla?')}}
+        OR p.Known_For = {{LLMQA('Which of these do you like best?')}}
+        """,
+        model=model,
+    )
+    assert smoothie.meta.num_values_passed == 0
+    assert smoothie.meta.num_generation_calls == 2
