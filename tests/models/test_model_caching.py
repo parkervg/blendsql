dcb137202468914b6ccdc77c020f9a47ef3b9ef2
parkervg <parkervg5@gmail.com>
2025-12-10 11:46:35 -0500

    fix test

--- tests/models/test_model_caching.py
+++ tests/models/test_model_caching.py
@@ -13,25 +13,29 @@ def bsql() -> BlendSQL:
 
 
 def test_llmmap_cache(bsql, model):
-    if not isinstance(model, ConstrainedModel):
-        # TODO: fix Unconstrained caching
-        pytest.skip()
-    model.caching = True
-    model.cache.clear()
-    query = """
-    SELECT p.Body, {{LLMMap('What is the sentiment of this post?', p.Body, options=('positive', 'negative'))}}
-    FROM posts p LIMIT 10
-    """
-    first = bsql.execute(
-        query,
-        model=model,
-    )
-    second = bsql.execute(
-        query,
-        model=model,
-    )
+    try:
+        if not isinstance(model, ConstrainedModel):
+            # TODO: fix Unconstrained caching
+            pytest.skip()
+        model.caching = True
+        model.cache.clear()
+        query = """
+        SELECT p.Body, {{LLMMap('What is the sentiment of this post?', p.Body, options=('positive', 'negative'))}}
+        FROM posts p LIMIT 10
+        """
+        first = bsql.execute(
+            query,
+            model=model,
+        )
+        second = bsql.execute(
+            query,
+            model=model,
+        )
 
-    assert first.meta.process_time_seconds > second.meta.process_time_seconds
-    assert second.meta.num_generation_calls == 0
-    model.reset_stats()
-    model.caching = False
+        assert first.meta.process_time_seconds > second.meta.process_time_seconds
+        assert second.meta.num_generation_calls == 0
+    except Exception:
+        raise
+    finally:
+        model.reset_stats()
+        model.caching = False
