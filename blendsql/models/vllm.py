26f48ba444a7eaeaecde58cc859aa9180a2e00ce
parkervg <parkervg5@gmail.comclear>
2026-02-05 17:36:15 -0500

    update

--- blendsql/models/vllm.py
+++ blendsql/models/vllm.py
@@ -10,6 +10,20 @@ DEFAULT_BODY = {"temperature": 0.0}
 
 
 class VLLM(ModelBase):
+    """Class for vLLM endpoints.
+
+    Args:
+        model_name_or_path: Name of the model
+        base_url: Base URL for http requests
+
+    Examples:
+        ```python
+        from blendsql.models import VLLM
+
+        model = VLLM("RedHatAI/gemma-3-12b-it-quantized.w4a16", base_url="http://localhost:8000/v1/")
+        ```
+    """
+
     def __init__(
         self,
         model_name_or_path: str,
